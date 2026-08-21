269ee031928de449a5fd61e4ccd71f6e505d0786
prakashprajapat2008 <prakash.prajapat.cg@gmail.com>
2026-08-21 17:04:41 +0530

    new file added

---
+++ about_python.py
@@ -0,0 +1,3 @@
+#This is my first python program and i am currentl learinig pyton language
+print("Print Hello") 
+print("My name is prakash ")
---
+++ welcome.py
@@ -0,0 +1,3 @@
+#This is my first python program
+print("hello world") 
+print("My name is prakash ")
