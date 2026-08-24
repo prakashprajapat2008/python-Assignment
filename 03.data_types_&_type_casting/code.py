a5c383340e7c8835cf769efdb573c0506d6e893f
prakashprajapat2008 <prakash.prajapat.cg@gmail.com>
2026-08-24 21:32:06 +0530

    new files add

---
+++ 03.data_types_&_type_casting/code.py
@@ -0,0 +1,74 @@
+#Q=24
+student_name = "prakash"
+student_age = 18
+student_height = 5.8
+is_student = True
+student_reasult = None
+print(student_name,type(student_name))
+print(student_age,type(student_age))
+print(student_height,type(student_height))
+print(is_student,type(is_student))
+print(student_reasult,type(student_reasult))
+
+
+#Q=25
+a = 50
+b = 50.0
+c = "50"
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+
+
+#Q=26
+a = True
+b = "True"
+print(type(a))
+print(type(b))
+
+
+#Q=27
+a = None
+b = "None"
+print(type(a))
+print(type(b))
+
+
+#Q=28
+value = 10
+print(type(value))
+value = 25
+print(type(value))
+value = "Hello"
+print(type(value))
+
+
+#Q=29
+product_name = "Laptop"
+product_quantity = 10
+product_price = 49999.99
+product_available = True
+product_discount = None
+print(type(product_name))
+print(type(product_quantity))
+print(type(product_price))
+print(type(product_available))
+print(type(product_discount))
+
+
+#Q=30
+a = 10
+b = 10.0
+c = "10"
+d = True
+e = "True"
+f = None
+g = "None"
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
+print(f, type(f))
+print(g, type(g))
+#Q=12
