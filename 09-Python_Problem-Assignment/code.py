c83172be8d9c436f24e27cac0fda8db932149425
prakashprajapat2008 <prakash.prajapat.cg@gmail.com>
2026-09-09 21:50:28 +0530

    file added

--- 09-Python_Problem-Assignment/code.py
+++ 09-Python_Problem-Assignment/code.py
@@ -159,16 +159,16 @@ else:
     print("Special character:")
 
 
-#13. Vowel or Consonant
-#Q=13 ---------------------------------------------------------------?
-C = input("Enter your character: ")
-if C!= 'a,e,i,o,u':
-    if C!= 'b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z':
-        print("Vowel")
-    else:
-        print("Consonant")
-else:
-    print("Invalid input")
+# #13. Vowel or Consonant
+# #Q=13 ---------------------------------------------------------------?
+# C = input("Enter your character: ")
+# if C!= 'a,e,i,o,u':
+#     if C!= 'b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z':
+#         print("Vowel")
+#     else:
+#         print("Consonant")
+# else:
+#     print("Invalid input")
 
 
 #14. Profit or Loss
@@ -305,7 +305,157 @@ else:
     print("dono wrong h")
 
 
+#24. Discount Calculator
+#Q=24 ------------------------------------------------------------------------------------------------?
+amount=int(input("Enetr your purchase amount:"))
+if 0<amount<=500:
+    discounta=amount*0
+    print("Discount: 0%" "\nDiscount amount:",discounta, "\nFinal amount:",amount-discounta)
+elif 500<=amount<=999:
+    discounta=(amount*5)/100
+    print("Discount: 5%" "\nDiscount amount:",discounta, "\nFinal amount:",amount-discounta)
+elif 1000<=amount<=1999:
+    discounta=(amount*10)/100
+    print("Discount: 10%" "\nDiscount amount:",discounta ,"\nFinal amount:",amount-discounta)
+elif 2000<=amount<=4999:
+    discounta=(amount*15)/100
+    print("Discount: 15%" "\nDiscount amount:",discounta ,"\nFinal amount:",amount-discounta)
+elif amount>=5000:
+    discounta=(amount*20)/100
+    print("Discount: 20%" "\nDiscount amount:",discounta ,"\nFinal amount:",amount-discounta)    
+else:
+    print("please enter valid amount")
 
+
+#25. Student Result System   
+#Q=25 ----------------------------------------------------------------------------------------------?
+Git=int(input("Enter your in Git subjects marks:"))
+Python=int(input("enter your in Python subjects marks: "))
+Github=int(input("enter your in Github subjects marks:"))
+if 0<Git<=100 and 0<Python<=100 and 0<Github<=100:
+    if Git<35 or Python<35 or Github<35:
+        print("Fail")
+    else:
+        total=Git+Github+Python
+        average=total/3
+        if average>=75:
+            print("Distinction")   
+        elif 60<=average<=74 :
+            print("First Class")
+        elif 50<=average<=59:
+            print("Second Class")
+        elif 35<=average<=49:
+            print("pass")
+        else:
+            print("not result found")
+
+#26. Date Validator
+#Q=26 ------------------------------------------------------------------------------------------------?
+Day=int(input("Enter your day:"))
+Month=int(input("Enter your month:"))
+Year=int(input("Enetr your year:"))
+if 0<Day<32 and 0<Month<13 and Year>0:
+    if Month==1 or Month==3 or Month==5 or Month==7 or Month==8 or Month==10 or Month==12:
+        print("valid")
+    elif(Month==4 or Month==6 or Month==9 or Month==11) and Day<31:
+        print("valid")
+    elif (Month==4 or Month==6 or Month==9 or Month==11) and Day==31:  
+        print("date is not valid")  
+    else:
+        if Month==2 and (Year%400==0 or Year%4==0) and Year%100!=0 and Day==29:  
+            print("valid")  
+        elif Month==2 and Day<29:
+            print("valid")
+        else:
+            print("date is not valid")    
+else:
+    print("date is not valid")                     
+
+#27. Time Validator
+#Q=27 -------------------------------------------------------------------------------------------------?
+hours=int(input("Enter Your in hours:"))
+minutes=int(input("Enter Your in minutes:"))
+seconds=int(input("Enter Your in seconds:"))
+if 0<hours<23:
+    if 0<minutes<59:
+        if 0<seconds<59:
+            print("valid time")
+else:
+            print("invalid time")
+
+
+#28. Youngest of Three People
+#Q=28 ---------------------------------------------------------------------------------------------------?
+Rahul = input("Enter Your in name:")
+age1 = int(input("Enter Your in age:"))
+Amit = input("Enter Your in name:")
+age2 = int(input("Enter Your in age:"))
+Jay = input("Enter Your in name:")
+age3=int(input("Enter Your in age:"))
+if age1<age2 and age1<age3:
+    print(f"{Rahul} is the youngest")
+elif age2<age1 and age2<age1:
+    print(f"{Amit} is the youngest")
+elif age3<age2 and age3<age2:
+    print(f"{Jay} is the youngest")
+else:
+    if age1==age2 and age2==age3 and age1!=age3:
+        print(f"{Rahul} and {ArithmeticError} same")
+    elif age2==age3 and age2!=age3 and age2==age1:
+        print(f"{Amit} and {Jay} same")  
+    else:
+        print("three persion same")      
+
+
+#29. Second Largest of Three Numbers
+#Q=29 ---------------------------------------------------------------------------------------------------?
+num1=int(input("Enter Your number 1:"))
+num2=int(input("Enter Your number 2:"))
+num3=int(input("Enter Your number 3:"))
+if num1>num2 and num1>num3:
+    if num2>num3:
+        print(f"Middle number is {num2}")
+    else:
+        print(f"Middle number is {num3}")
+elif num2>num1 and num2>num3:
+    if num1>num3:
+        print(f"Middle number is {num1}")  
+    else:
+        print(f"Middle number is {num3}") 
+elif num3>num1 and num3>num2:
+    if num1>num2:
+        print(f"Middle number is {num1}")
+    else:
+        print(f"Middle number is {num2}")                     
+elif num1==num2==num3:
+    print(f"Middle number is {num2}")
+else:
+    if num1==num2:
+        print(f"Middle number is {num2}")
+    elif num1==num3:
+        print(f"Middle number is {num1}")
+    else:
+        print(f"Middle number is {num2}") 
+
+
+#30. Complete Scholarship Decision ⭐
+#Q=30 ------------------------------------------------------------------------------------------------?
+student_age=int(input("Enter Your student age:"))
+marks=int(input("Enter Your student marks:"))
+family_income=int(input("Enter Your family income:"))
+attendance_percentage=int(input("Enter Your student attendance percentage:"))
+if 18<=student_age<=25 and 85<=marks<=100 and 0<=family_income<=30000 and  75<=attendance_percentage<=100:
+    print("Scholarship Approved")
+elif not 18<=student_age<=25:
+    print("Scholarship Rejected \nReason: Student age is not satisfied")
+elif not 85<=marks<=100:
+    print("Scholarship Rejected \nReason: Marks below 85")
+elif not 0<=family_income<=30000:
+    print("Scholarship Rejected \nReason: family income more than 30000") 
+elif not 75<=attendance_percentage<=100:
+    print("Scholarship Rejected \nReason: attendance is less than 75%")      
+else:
+    print("none")
 
 
 
